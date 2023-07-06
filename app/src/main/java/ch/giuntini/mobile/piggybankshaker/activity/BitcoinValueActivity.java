e89b61e0af9faf0cef7db39418717a6b7df83ca3
Medox36 <lorenzo@giuntini.ch>
2023-07-06 16:06:51 +0200

    realised View according to Mockup for BitcoinValueView

---
+++ app/src/main/java/ch/giuntini/mobile/piggybankshaker/activity/BitcoinValueActivity.java
@@ -0,0 +1,16 @@
+package ch.giuntini.mobile.piggybankshaker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.giuntini.mobile.piggybankshaker.R;
+
+public class BitcoinValueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bitcoin_value);
+    }
+}
