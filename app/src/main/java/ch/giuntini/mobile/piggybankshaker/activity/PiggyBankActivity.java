077c54931bffeaa0308e295eceee74b173c48947
Medox36 <lorenzo@giuntini.ch>
2023-07-06 13:23:40 +0200

    realised View according to Mockup for PiggyBankView

---
+++ app/src/main/java/ch/giuntini/mobile/piggybankshaker/activity/PiggyBankActivity.java
@@ -0,0 +1,16 @@
+package ch.giuntini.mobile.piggybankshaker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.giuntini.mobile.piggybankshaker.R;
+
+public class PiggyBankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piggy_bank);
+    }
+}
