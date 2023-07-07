59e8cfa9091bfced8e6c33323ee8c16d10e58adf
Medox36 <lorenzo@giuntini.ch>
2023-07-07 14:17:29 +0200

    added Unittests according to test cases

--- app/src/test/java/ch/giuntini/mobile/piggybankshaker/ExampleUnitTest.java
+++ app/src/test/java/ch/giuntini/mobile/piggybankshaker/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package ch.giuntini.mobile.piggybankshaker;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import ch.giuntini.mobile.piggybankshaker.service.DataManagerService;
+import ch.giuntini.mobile.piggybankshaker.service.SlotMachineService;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,55 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private DataManagerService dataManagerService;
+    private SlotMachineService slotMachineService;
+
+    @Before
+    public void init() {
+        dataManagerService = new DataManagerService();
+        slotMachineService = new SlotMachineService();
+    }
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void savingNumberOfCoinsAndBitcoins() {
+        dataManagerService.incrementCoinsBy(48);
+        dataManagerService.incrementBitcoinsBy(8.4775f);
+
+        assertEquals(dataManagerService.getCoins(), 48);
+        assertEquals(dataManagerService.getBitcoins(), 8.4775f, 0.01);
+    }
+
+    @Test
+    public void randomSymbolSelection() {
+        Symbol[] spin1 = slotMachineService.spin();
+        Symbol[] spin2 = slotMachineService.spin();
+
+        int cnt = 0;
+        for (int i = 0; i < 4; i++) {
+            if (spin1[i] == spin2[i]) {
+                cnt++;
+            }
+        }
+
+        // should the symbols be the same by any chance
+        // we re-spin and check if they still are the same
+        if (cnt == 3) {
+            spin1 = slotMachineService.spin();
+            spin2 = slotMachineService.spin();
+            cnt = 0;
+            for (int i = 0; i < 4; i++) {
+                if (spin1[i] == spin2[i]) {
+                    cnt++;
+                }
+            }
+        }
+
+        assertNotEquals(cnt, 3);
     }
 }
